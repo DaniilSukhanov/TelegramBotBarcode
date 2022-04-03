695183f0e1912baeb27f4be6e37922d89722f314
DaniilSukhanov <82699248+DaniilSukhanov@users.noreply.github.com>
2022-04-03 17:08:40 +0300

    Добавление логирования.

--- handlers/users/barcode.py
+++ handlers/users/barcode.py
@@ -1,3 +1,4 @@
+import logging
 from aiogram import types
 from aiogram.utils import exceptions
 
@@ -8,21 +9,26 @@ import PIL
 
 @dp.message_handler(content_types=types.ContentType.PHOTO)
 async def decode_photo(message: types.Message):
-    """"""
+    """Обработчик декодирования фото со штрих кодом."""
     file_id = message.photo[-1].file_id
     try:
         bytes_file = await bot.download_file_by_id(file_id)
         data_barcode = decode_image(bytes_file)
+        logging.info('Image decoded successfully.')
+        logging.debug(f'Image decoded: {data_barcode}')
         await message.answer(data_barcode)
     except PIL.UnidentifiedImageError:
+        logging.info('Images are not the correct format.')
         await message.answer(
-            'Неправильный тип файла.'
+            'Неправильный формат изображения.'
         )
     except IndexError:
+        logging.info('Barcode on image not found.')
         await message.answer(
             'Штрих-код не найден.'
         )
     except exceptions.FileIsTooBig:
+        logging.info('File is too big.')
         await message.answer(
             'Файл слишком большой.'
         )
